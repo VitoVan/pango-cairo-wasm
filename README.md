61882b952c58c706a009ff0873022dc98be73fc8
VitoVan <vito@sdf.org>
2023-04-23 06:10:58 +0000

    add sample C files

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# pango-cairo-wasm
-PangoCairo on the Web
+# PangoCairo on the Web
+
+Compile a Cairo / Pango / PangoCairo based application from C to WebAssembly.
+
+## Usage
+
+### Setup emcc & pkg-config
+
+#### with Docker
+
+```bash
+alias emcc="docker run --rm --name pcwa -v $(pwd):/app -w=/app vitovan/pango-cairo-wasm emcc"
+alias pkg-config="docker run --rm --name pcwa -v $(pwd):/app -w=/app vitovan/pango-cairo-wasm pkg-config"
+# give it a try
+emcc --help
+pkg-config --libs --cflags pangocairo
+```
+
+#### from Source
+
+This build.sh only compiles on Fedora, if you can't get one, please modify it before execution.
+
+```bash
+git clone git@github.com:VitoVan/pango-cairo-wasm.git
+cd pango-cairo-wasm
+git submodule init
+git submodule update
+echo -e "export magicdir=${HOME}/pango-cairo-wasm-magic\n$(cat env.sh)" > env.sh
+bash build.sh
+```
+
+Wait for 20 minutes or more. If all went well, execute:
+
+```bash
+. env.sh
+# give it a try
+emcc --help
+pkg-config --libs --cflags pangocairo
+```
+
+### Compile C Code
+
+#### Cairo with no pthread
+
+```bash
+export CAIRO_FLAGS="$(pkg-config --libs --cflags glib-2.0, gobject-2.0, cairo, pixman-1, freetype2, fontconfig, cairo, expat, harfbuzz) -s USE_SDL=2 -s USE_PTHREADS=0 -s ASYNCIFY"
+
+curl -sSLo https://raw.githubusercontent.com/VitoVan/pango-cairo-wasm/main/hello-cairo.c
+emcc $CAIRO_FLAGS hello-cairo.c -o hello-cairo.html
+```
+
+#### PangoCairo with pthread
+
+```bash
+export PANGOCAIRO_FLAGS="$(pkg-config --libs --cflags glib-2.0, gobject-2.0, cairo, pixman-1, freetype2, fontconfig, cairo, expat, harfbuzz, pangocairo) -s USE_SDL=2 -s EMULATE_FUNCTION_POINTER_CASTS -s PTHREAD_POOL_SIZE=10 -s USE_PTHREADS=1 -s ASYNCIFY"
+curl -sSLo https://raw.githubusercontent.com/VitoVan/pango-cairo-wasm/main/hello-pangocairo.c
+# get yourself the fonts
+mkdir ./fonts
+cd fonts
+curl -sSLo https://raw.githubusercontent.com/VitoVan/pango-cairo-wasm/main/fonts/OpenSans-VariableFont_wdth,wght.ttf
+curl -sSLo https://raw.githubusercontent.com/VitoVan/pango-cairo-wasm/main/fonts/OpenSans-Italic-VariableFont_wdth,wght.ttf
+cd ..
+# compile
+emcc $PANGOCAIRO_FLAGS --embed-file ./fonts@/usr/share/fonts/ hello-pangocairo.c -o hello-pangocairo.html
+```
+
+#### Start the debug server
+
+```bash
+curl -sSLo https://raw.githubusercontent.com/VitoVan/pango-cairo-wasm/main/wasm-server
+chmod +x wasm-server
+./wasm-server
+```
+
+open the following links in your browser:
+
+- http://localhost:4242/hello-cairo.html
+- http://localhost:4242/hello-pangocairo.html
---
+++ hello-pangocairo.c
@@ -0,0 +1,164 @@
+/*
+the following file is
+copied and modified from:
+https://github.com/tsuu32/sdl2-cairo-example/blob/master/sdl2-cairo.c
+*/
+#include <emscripten.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <cairo/cairo.h>
+#include <pango/pangocairo.h>
+
+int
+main(int argc, char *argv[])
+{
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window *window = SDL_CreateWindow("An SDL2 window",
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        640,
+                                        480,
+                                        SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window,
+                                              -1,
+                                              SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  int window_width;
+  int window_height;
+  SDL_GetWindowSize(window, &window_width, &window_height);
+
+  printf("window_width=%d\n"
+         "window_height=%d\n",
+         window_width, window_height);
+
+  int renderer_width;
+  int renderer_height;
+  SDL_GetRendererOutputSize(renderer, &renderer_width, &renderer_height);
+
+  printf("renderer_width=%d\n"
+         "renderer_height=%d\n",
+         renderer_width, renderer_height);
+
+  int cairo_x_multiplier = renderer_width / window_width;
+  int cairo_y_multiplier = renderer_height / window_height;
+
+  SDL_Surface *sdl_surface = SDL_CreateRGBSurface(0,
+                                                  renderer_width,
+                                                  renderer_height,
+                                                  32,
+                                                  0x00ff0000,
+                                                  0x0000ff00,
+                                                  0x000000ff,
+                                                  0);
+
+  printf("sdl_surface->w=%d\n"
+         "sdl_surface->h=%d\n"
+         "sdl_surface->pitch=%d\n",
+         sdl_surface->w, sdl_surface->h, sdl_surface->pitch);
+  printf("sdl_surface->format->format=%s\n",
+         SDL_GetPixelFormatName(sdl_surface->format->format));
+
+  cairo_surface_t *cr_surface = cairo_image_surface_create_for_data((unsigned char *)sdl_surface->pixels,
+                                                                       CAIRO_FORMAT_RGB24,
+                                                                       sdl_surface->w,
+                                                                       sdl_surface->h,
+                                                                       sdl_surface->pitch);
+
+  cairo_surface_set_device_scale(cr_surface, cairo_x_multiplier, cairo_y_multiplier);
+
+  cairo_t *cr = cairo_create(cr_surface);
+
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+  SDL_RenderClear(renderer);
+
+  // White background with SDL2 API
+  // SDL_FillRect(sdl_surface, NULL, SDL_MapRGB(sdl_surface->format, 255, 255, 255));
+
+  // White background with cairo API
+  cairo_set_source_rgba(cr, 1, 1, 1, 1.0);
+  cairo_rectangle(cr, 0, 0, 640, 480);
+  cairo_fill(cr);
+
+  double xc = 320.0;
+  double yc = 240.0;
+  double radius = 200.0;
+  double angle1 = 45.0  * (M_PI/180.0);
+  double angle2 = 180.0 * (M_PI/180.0);
+
+  cairo_set_source_rgba(cr, 0, 0, 0, 1.0);
+  cairo_set_line_width(cr, 10.0);
+  cairo_arc(cr, xc, yc, radius, angle1, angle2);
+  cairo_stroke(cr);
+
+  cairo_set_source_rgba(cr, 1, 0.2, 0.2, 0.6);
+  cairo_set_line_width(cr, 6.0);
+
+  cairo_arc(cr, xc, yc, 10.0, 0, 2*M_PI);
+  cairo_fill(cr);
+
+  cairo_arc(cr, xc, yc, radius, angle1, angle1);
+  cairo_line_to(cr, xc, yc);
+  cairo_arc(cr, xc, yc, radius, angle2, angle2);
+  cairo_line_to(cr, xc, yc);
+  cairo_stroke(cr);
+
+  // PangoCairo
+
+  PangoFontDescription *font_description = pango_font_description_new();
+  pango_font_description_set_family(font_description, "Open Sans");
+  pango_font_description_set_weight(font_description, PANGO_WEIGHT_BOLD);
+  pango_font_description_set_absolute_size(font_description, 42 * PANGO_SCALE);
+
+  PangoLayout *layout = pango_cairo_create_layout(cr);
+  pango_layout_set_font_description(layout, font_description);
+
+  // pango_layout_set_text(layout, "Hello, pango", -1);
+  pango_layout_set_markup(layout,
+                          "<span foreground='white' background='black'><i> Hello </i></span>, <span foreground='blue' "
+                          "size='x-large'>pango</span><span foreground='red' "
+                          "size='large'>markup</span>",
+                          -1);
+
+  cairo_set_source_rgb(cr, 1, 1, 1.0);
+  cairo_move_to(cr, 50, 50.0);
+  pango_cairo_show_layout(cr, layout);
+
+  g_object_unref(layout);
+  pango_font_description_free(font_description);
+
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, sdl_surface);
+  SDL_FreeSurface(sdl_surface);
+
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
+
+  bool done = false;
+  while (!done) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        done = true;
+        break;
+      default:
+        break;
+      }
+    }
+    // SDL_Delay(100);
+    emscripten_sleep(100);
+  }
+
+  cairo_destroy(cr);
+  cairo_surface_destroy(cr_surface);
+
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+
+  return 0;
+}
