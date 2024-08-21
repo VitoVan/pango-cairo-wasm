71fffdf3e0f21e3c2ba02726ae845b1be41ade31
Jacob Raaff <103148136+notsilverhoft@users.noreply.github.com>
2024-08-20 20:06:28 -0600

    Update env.sh

--- env.sh
+++ env.sh
@@ -16,8 +16,8 @@ export PKG_CONFIG_LIBDIR=${magicprefix}/lib/
 export CHOST="wasm32-unknown-linux"
 export ax_cv_c_float_words_bigendian=no
 
-${magicdir}/emsdk/emsdk install latest
-${magicdir}/emsdk/emsdk activate latest
+./${magicdir}/emsdk/emsdk install latest
+./${magicdir}/emsdk/emsdk activate latest
 source ${magicdir}/emsdk/emsdk_env.sh
 
 export MESON_CROSS="${magicdir}/emscripten-crossfile.meson"
